e48bd3653a7bd8df8bc902d57a61a8823b558982
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2026-01-03 21:45:37 +0100

    chore(nvim): added go.nvim

---
+++ dot_config/nvim/lua/plugins/go.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = { "go", "gomod" },
+    config = function()
+      require("go").setup({
+        lsp_cfg = true,
+        lsp_on_attach = true,
+      })
+    end,
+  },
+}
