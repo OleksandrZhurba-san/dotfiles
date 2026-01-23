6fed82e639eecfd103556a8ed059e120b8ece3f5
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2026-01-23 10:35:30 +0100

    update

--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -9,9 +9,19 @@ return {
     ft = { "go", "gomod" },
     config = function()
       require("go").setup({
-        lsp_cfg = true,
-        lsp_on_attach = true,
-        lsp_inlay_hints = false,
+        lsp_cfg = false,
+        lsp_on_attach = false,
+        lsp_inlay_hints = {
+          enabled = false,
+        },
+      })
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        group = format_sync_grp,
+        callback = function()
+          require("go.format").goimports()
+        end,
       })
     end,
   },
